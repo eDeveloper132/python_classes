6c15d64fb3cd0066358c1099043b8a800b2b023f
eDeveloper132 <1375219.aptechiic@gmail.com>
2025-05-07 21:02:56 +0500

    some improvements

--- Object/main.py
+++ Object/main.py
@@ -3,13 +3,18 @@ class Person:
     name: str
     age: int
     gender: str
+    secret: str
 
-    def __init__(self, name: str, age: int, gender: str):
+    def __init__(self, name: str, age: int, gender: str, secret: str):
         self.name = name
         self.age = age
         self.gender = gender
+        self.__secret = secret
+    def get_secret(self):
+        return self.__secret
 
-person1 = Person("John", 36, "male")
+person1 = Person("John", 36, "male", "secret")
 print(person1.name)
-print(person1.age)
+print(person1.age) 
 print(person1.gender)
+print(person1.get_secret())
