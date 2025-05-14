53104265509b6f82ac24af64c2f7148bbd343c86
eDeveloper132 <1375219.aptechiic@gmail.com>
2025-05-14 20:30:54 +0500

    Spaces Removed

--- Polymorphism/main.py
+++ Polymorphism/main.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-
 class Animal(ABC):
     @abstractmethod
     def speak(self) -> None:
@@ -7,24 +6,20 @@ class Animal(ABC):
     @abstractmethod
     def eat(self) -> None:
         pass
-
 class Dog(Animal):
     def speak(self) -> None:
         print(type(self), ":  Woof!")
     def eat(self) -> None:
         print(type(self), ": Meat")
-
 class Cat(Animal):
     def speak(self) -> None:
         print(type(self), ":  Meow!")
     def eat(self) -> None:
         print(type(self), ": Meat")
-
 def animal_sound(animal: Animal) -> None:
     animal.speak()
 def animal_eat(animal: Animal) -> None:
     animal.eat()
-
 dog = Dog()
 cat = Cat()
 animal_sound(dog)  # Output: Woof!
